5b1f16792e8aaf90cce899bbfb0f1a35b6e6749b
Michal Wagner <xwagner@fi.muni.cz>
2025-11-30 12:24:28 +0100

    Add group to navigation

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -12,6 +12,7 @@ const ClientNavigation = ({ session }: { session: Session | null }) => {
 	const links = [
 		{ href: '/', label: 'Home' },
 		{ href: '/movielists', label: 'My Movies' },
+		{ href: '/groups', label: 'Groups' },
 		{ href: 'https://www.imdb.com/', label: 'IMDb', external: true },
 		{ href: 'https://www.csfd.cz/', label: 'ČSFD', external: true },
 		{ href: '/search', label: 'Search' } //TODO include input field
