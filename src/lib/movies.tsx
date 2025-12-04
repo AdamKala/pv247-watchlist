38b696c8b9814bf0ab45a7d22ab304b6a41a16d9
Adam Kala <adamkala07@gmail.com>
2025-12-04 12:51:16 +0100

    Adjusted lint and connection.

--- src/lib/movies.tsx
+++ src/lib/movies.tsx
@@ -1,18 +1,17 @@
-import {movies, watchlistItems, watchlists} from "@/db/schema";
+import { type movies, type watchlistItems, type watchlists } from '@/db/schema';
 
 export type Movie = typeof movies.$inferSelect;
 export type NewMovie = typeof movies.$inferInsert;
 export type MovieSearchItemProps = {
-    title: string;
-    posterUrl: string;
-    id: number;
-    origins: string[];
-    type: string;
-    year: string;
-}
+	title: string;
+	posterUrl: string;
+	id: number;
+	origins: string[];
+	type: string;
+	year: string;
+};
 
-
-export type WatchlistItem = typeof watchlistItems.$inferSelect;  // řádek z DB
+export type WatchlistItem = typeof watchlistItems.$inferSelect; // řádek z DB
 export type NewWatchlistItem = typeof watchlistItems.$inferInsert; // pro insert
 
 export type WatchList = typeof watchlists.$inferSelect;
